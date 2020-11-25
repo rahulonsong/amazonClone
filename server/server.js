3273ddd73d8e07deb4bffbc8c75719be551aa347
Rahul Kanakath <rahulkraj123@gmail.com>
2020-11-24 22:59:37 -0500

    modified:   server/package-lock.json :x
    qq.
    
    :X

--- server/server.js
+++ server/server.js
@@ -1,32 +1,37 @@
-const express = require('express') 
+const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+// const User = require('./models/user')
 
+dotenv.config()
 
 const app = express()
 
+mongoose.connect(process.env.DATABASE,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Connected to Database!')
+    }
+  })
+
 // Middleware
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
-// GET - Retrieve data from the server
-app.get('/', (req, res, next) => {
-    res.json("Hello Amazon Clone!")
-})
-
-
-// POST - send data from front end to back end
-app.post('/', (req, res, next) => {
-    console.log(req.body.name);
-})
-
+// require API
+const productRoutes = require('./routes/product')
+app.use('/api', productRoutes)
 
 app.listen(3000, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Listening on PORT 3000!");
-    }
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Listening on PORT 3000!')
+  }
 })
